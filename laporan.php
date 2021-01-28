be87d79674fa05ef604a3193e7e1e44a9d724e65
rodesta2212 <rodesta2212@gmail.com>
2021-01-28 19:12:08 +0700

    laporan view

--- laporan.php
+++ laporan.php
@@ -24,18 +24,18 @@
 		<div class="pd-ltr-20 xs-pd-20-10">
 			<div class="min-height-200px">
 				<div class="row">
-					<div class="col-md-6 mb-30">
+					<div class="col-md-12 mb-30">
 						<div class="pd-20 card-box height-100-p">
-							<h4 class="h4 text-blue">Pie Chart</h4>
+							<h4 class="h4 text-blue">Laporan Jumlah Pasien Setiap Poli</h4>
 							<div id="piechart"></div>
 						</div>
 					</div>
-					<div class="col-md-6 mb-30">
+					<!-- <div class="col-md-6 mb-30">
 						<div class="pd-20 card-box height-100-p">
 							<h4 class="h4 text-blue">Radial Bar Chart</h4>
 							
 						</div>
-					</div>
+					</div> -->
 				</div>
 			</div>
 			<!-- footer -->
@@ -47,8 +47,6 @@
 	<script src="vendors/scripts/script.min.js"></script>
 	<script src="vendors/scripts/process.js"></script>
 	<script src="vendors/scripts/layout-settings.js"></script>
-	<script src="src/plugins/apexcharts/apexcharts.min.js"></script>
-	<script src="vendors/scripts/apexcharts-setting.js"></script>
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
@@ -61,15 +59,13 @@
         function drawChart() {
         var data = google.visualization.arrayToDataTable([
         ['Task', 'Hours per Day'],
-        ['Work', 8],
-        ['Eat', 2],
-        ['TV', 4],
-        ['Gym', 2],
-        ['Sleep', 8]
+        ['Poli Umum', 83],
+        ['Poli Ibu dan Anak', 46],
+        ['Poli Gigi', 40]
         ]);
 
         // Optional; add a title and set the width and height of the chart
-        var options = {'title':'My Average Day', 'width':550, 'height':400};
+        var options = {'title':'Jumlah Rata-Rata Perbulan'};
 
         // Display the chart inside the <div> element with id="piechart"
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
